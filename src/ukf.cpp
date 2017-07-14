7a46cb002afb4e749a5a151125d38d8cf221163c
Muhammad Saad Nawaz <saadnawaz27@yahoo.com>
2017-07-14 21:41:17 +0200

    std_a_ and std_yawd_ adjusted to 0.2 each

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -28,10 +28,10 @@ UKF::UKF() {
   P_ = MatrixXd(n_x_, n_x_);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2 //incorrectly set by Udacity
-  std_a_ = 0;
+  std_a_ = 0.2;
 
   // Process noise standard deviation yaw acceleration in rad/s^2 //incorrectly set by Udacity
-  std_yawdd_ = 0;
+  std_yawdd_ = 0.2;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
