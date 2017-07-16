98edbb21e86e85eebebb487ae5162390331e63e8
Muhammad Saad Nawaz <saadnawaz27@yahoo.com>
2017-07-16 04:41:53 +0200

    x changed in covariance prediction step

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -256,7 +256,7 @@ void UKF::Prediction(double delta_t) {
   //predict state covariance matrix
   P.fill(0);
   for(int i=0; i<2*n_aug_+1; i++){
-	  VectorXd x_diff = Xsig_pred_.col(i) - Xsig_pred_.col(0);
+	  VectorXd x_diff = Xsig_pred_.col(i) - x;
 	  while(x_diff(3) > M_PI)
 		  x_diff(3) -= 2*M_PI;
 	  while(x_diff(3) < -M_PI)
